7caaefac9f53f96e8bc2d1d0734a04bbb470e690
LuizKramer <luizeduardockramer@gmail.com>
2023-07-21 10:27:57 -0300

    Finish first project

--- index.js
+++ index.js
@@ -4,11 +4,14 @@
 // init project
 var express = require('express');
 var app = express();
-
+let bodyParser = require('body-parser');
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }),
+  bodyParser.urlencoded({
+    extended: false
+  }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +21,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+  res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/:date?', (req, res) => {
+  const months = [
+    "Jan", "Feb", "Mar","Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ]
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let date = new Date(req.params.date);
+  let formattedDate = `${daysOfWeek[date.getUTCDay()]} ,${date.getDate() +1} ${months[date.getUTCMonth()]} ${date.getFullYear()} ${date.getUTCHours().toString().padStart(2, '0')}:${date.getUTCMinutes().toString().padStart(2, '0')}:${date.getUTCSeconds().toString().padStart(2, '0')}`
+  res.json({
+    unix: date.getTime(),
+    utc: formattedDate
+  })
+})
 
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
+
   console.log('Your app is listening on port ' + listener.address().port);
 });
